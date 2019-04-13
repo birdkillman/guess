d72c8c4865c0f778c882e32ec21f796aee759a2e
Hill Yang <birdkillman@gmail.com>
2019-04-13 13:55:05 -0400

    add new one

--- guess.py
+++ guess.py
@@ -1,5 +1,9 @@
 import random
-r = random.randint(1, 100)
+a = input('请输入最小范围： ')
+b = input('请输入最大范围： ')
+a = int(a)
+b = int(b)
+r = random.randint(a, b)
 x = 1
 while True:
 	print('第', x, '次机会')
